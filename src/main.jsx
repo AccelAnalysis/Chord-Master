70846d079ec520168ce4096140a7e63faabc8b10
AccelAnalysis <jholman@accelanalysis.com>
2026-07-16 13:03:10 -0400

    Mount progression loop control

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import LoopController from './components/LoopController.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <LoopController />
   </React.StrictMode>,
 );
